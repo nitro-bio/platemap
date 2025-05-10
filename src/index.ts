0e26563f78afae4d55f3ae13c402664a2b2ce005
Nishant Jha <github@nishantjha.org>
2025-05-10 16:56:22 -0700

    [wip]

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
-import { Plate } from "./Plate/Plate";
-import { usePlateReducer } from "./Plate/hooks/usePlateReducer";
-import type {
+export { Plate } from "./Plate/Plate";
+export {
+  usePlateReducer,
+  type PlateState,
+  type PlateActions,
+  type UsePlateParams,
+} from "./Plate/hooks/usePlateReducer";
+export type {
   WellAnnotation,
   PlateSize,
   AnnotationStyle,
   PlateSelection,
 } from "./Plate/schemas";
-
-export { Plate, usePlateReducer };
-export type { WellAnnotation, PlateSize, AnnotationStyle, PlateSelection };
+export {
+  BLUE_STYLE,
+  RED_STYLE,
+  GREEN_STYLE,
+  PURPLE_STYLE,
+  CYAN_STYLE,
+  ORANGE_STYLE,
+  GRAY_STYLE,
+} from "./Plate/schemas";
+export {
+  getRowLabel,
+  getRowLabels,
+  getColLabel,
+  plateSizeToRowsCols,
+  getEdgeWells,
+  randomizeWellAnnotations,
+  indexToExcelCell,
+  wellAnnotationsToCSV,
+  columnsToWells,
+  csvCellToIndex,
+  excelCellToIndex,
+  getExcelLabelForWells,
+  rowsToWells,
+  wellAnnotationsToList,
+} from "./Plate/utils";
